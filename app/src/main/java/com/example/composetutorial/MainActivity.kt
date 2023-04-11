9c9d7e90368946e8909dac2e1dedd81e3a35cf8c
Joshua Thieme <jomthieme@gmail.com>
2023-04-10 22:22:17 -0600

    Enabled Light / Dark Mode Previews

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.composetutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -69,6 +70,13 @@ fun MessageCard(msg : Message) {
         }
     }
 }
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+
 @Preview
 @Composable
 fun PreviewMessageCard() {
