f90c157bf7860ca68f86c4d34301808fb68b65cf
Joshua Thieme <jomthieme@gmail.com>
2023-04-10 22:02:17 -0600

    Styled the MessageCard

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,8 +30,26 @@ data class Message(val author : String, val body : String)
 
 @Composable
 fun MessageCard(msg : Message) {
-    Text(text = msg.author)
-    Text(text = msg.body)
+    // Add padding around the image
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(painter = painterResource(R.drawable.ic_launcher_foreground),
+            contentDescription = "Contact Profile Picture",
+            modifier = Modifier
+                // set image size to 40 dp
+                .size(40.dp)
+                // set the image to be a circle
+                .clip(CircleShape)
+        )
+        // add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = msg.author)
+            // add a vertical space between the author and message
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
 }
 @Preview
 @Composable
